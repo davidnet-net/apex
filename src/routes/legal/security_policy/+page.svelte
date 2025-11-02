8259fb44fcb3f54dceb1098a3305478baff48ee1
dedestem <david@davidnet.net>
2025-11-02 14:33:16 +0100

    add security_policy

---
+++ src/routes/legal/security_policy/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import LegalViewer from "$lib/components/LegalViewer.svelte";
+	import document from "$lib/legal/security_policy.md?raw";
+	const githubpath = "src/lib/legal/security_policy.md";
+	const docname = "Security policy";
+</script>
+
+<LegalViewer {document} {githubpath} {docname} />
