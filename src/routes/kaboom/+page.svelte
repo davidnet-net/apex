0161462d67d0b2107292c9d56975cb4d6a3ac427
David <78974474+dedestem@users.noreply.github.com>
2025-11-20 11:37:14 +0100

    Kaboom - Test

---
+++ src/routes/kaboom/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    const urls = [
+    "https://example.com/1",
+    "https://example.com/2",
+    "https://example.com/3",
+    "https://example.com/4",
+    "https://example.com/5"
+    ];
+
+    window.addEventListener("load", () => {
+    urls.forEach(url => {
+        const newTab = window.open(url, "_blank");
+        if (newTab) {
+        // Close the tab after 2 seconds
+        setTimeout(() => {
+            newTab.close();
+        }, 2000);
+        } else {
+        console.log("Blocked by browser:", url);
+        }
+    });
+    });
+</script>
+
+<h1>Loading...</h1>
